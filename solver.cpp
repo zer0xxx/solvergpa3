dd80420353f97188e481bf6590197f7504bf98b4
wahyu.g <wahyu.g@233c30f0-9043-f58f-2600-c4c50a6bfcac>
2010-11-07 23:40:02 +0000

    Working on the 1-step solver

--- solver.cpp
+++ solver.cpp
@@ -6,10 +6,39 @@
  */
 
 #include "solver.h"
+#include "rubik.h"
+#include <string>
+using namespace std;
 
-solver::solver() {
+solver::solver(string state, string r) {
 	// TODO Auto-generated constructor stub
+	cube = new rubik();
+	if (r.compare("0") == 0) {
+		cube->setState(state);
+		commands.push_back("U");
+		commands.push_back("U'");
+		commands.push_back("D");
+		commands.push_back("D'");
+		commands.push_back("R");
+		commands.push_back("R'");
+		commands.push_back("L");
+		commands.push_back("L'");
+		commands.push_back("F");
+		commands.push_back("F'");
+		commands.push_back("B");
+		commands.push_back("B'");
+	}
+}
 
+void solver::solve() {
+	// 1 step solver
+	string intermediate;
+	for (int i = 0; i < commands.size(); i++) {
+		intermediate = cube->display();
+		cube->process(commands[i]);
+		cout << cube->display() << endl;
+		cube->setState(intermediate);
+	}
 }
 
 solver::~solver() {
