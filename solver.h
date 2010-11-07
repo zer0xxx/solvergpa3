0c1b83ed33f1376b5e91f835eb1ed240d2d938a2
wahyu.g <wahyu.g@233c30f0-9043-f58f-2600-c4c50a6bfcac>
2010-11-07 22:27:51 +0000

    Program converted from Java to C++

---
+++ solver.h
@@ -0,0 +1,17 @@
+/*
+ * solver.h
+ *
+ *  Created on: Nov 7, 2010
+ *      Author: gbudiman
+ */
+
+#ifndef SOLVER_H_
+#define SOLVER_H_
+
+class solver {
+public:
+	solver();
+	virtual ~solver();
+};
+
+#endif /* SOLVER_H_ */
